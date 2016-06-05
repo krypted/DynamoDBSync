136cd0ae034b6811e3c922d373a8110bb3821201
krypted <krypted@me.com>
2016-06-05 17:57:15 -0500

    Update Constants.swift

--- MacMan/Constants.swift
+++ MacMan/Constants.swift
@@ -1,7 +1,7 @@
 import Foundation
 
-let CognitoRegionType = AWSRegionType.USEast1
-let DefaultServiceRegionType = AWSRegionType.USEast1
-let CognitoIdentityPoolId = "us-east-1:3d7eb036-cbc8-4fed-899e-73647150a7ac"
+let CognitoRegionType = 
+let DefaultServiceRegionType = 
+let CognitoIdentityPoolId = ""
 let MacManDynamoDBTableName = "MacMan"
 let MacManVersion = "1.0"
